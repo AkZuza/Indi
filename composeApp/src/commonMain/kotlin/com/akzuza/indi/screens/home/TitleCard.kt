e863dc34a3609bb1f3b63b7e3a71051f70cb003c
AkZuza <akhiljose@gmail.com>
2025-08-03 21:13:18 +0530

    reposition menubar

--- composeApp/src/commonMain/kotlin/com/akzuza/indi/screens/home/TitleCard.kt
+++ composeApp/src/commonMain/kotlin/com/akzuza/indi/screens/home/TitleCard.kt
@@ -1,12 +1,22 @@
 package com.akzuza.indi.screens.home
 
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.isSecondaryPressed
@@ -31,69 +42,87 @@ fun TitleCard(
 ) {
 
     val friendlyTitle = title.title.substring(
-        0, title.title.indexOfLast { '.' == it  }
+        0, title.title.indexOfLast { '.' == it }
     )
 
     var showExtraMenu by remember { mutableStateOf(false) }
 
-    Box {
-        OutlinedCard (
-            modifier = Modifier.fillMaxSize()
-                .combinedClickable(
-                    onLongClick = {
-                        showExtraMenu = true
-                    },
-                    hapticFeedbackEnabled = true,
-                    onClick = {
-                        openReader()
-                    }
-                )
-                .pointerInput(
-                    Unit
-                ) {
-                    awaitPointerEventScope {
-                        while (true) {
-                            val event = awaitPointerEvent()
-                            if(event.type == PointerEventType.Press && event.buttons.isSecondaryPressed) {
-                                showExtraMenu = true
-                            }
+    OutlinedCard(
+        modifier = Modifier.fillMaxSize()
+            .combinedClickable(
+                onLongClick = {
+                    showExtraMenu = true
+                },
+                hapticFeedbackEnabled = true,
+                onClick = {
+                    openReader()
+                }
+            )
+            .pointerInput(
+                Unit
+            ) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        if (event.type == PointerEventType.Press && event.buttons.isSecondaryPressed) {
+                            showExtraMenu = true
                         }
                     }
                 }
+            }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(12.dp)
         ) {
-            Column (
-                modifier = Modifier.fillMaxSize().padding(8.dp)
-            ) {
+            Row {
                 Text(
                     "$friendlyTitle",
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
                 )
+
+                Box (
+                    modifier = Modifier.width(20.dp)
+                ) {
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    TitleDropDownMenu(
+                        modifier = Modifier.align(Alignment.BottomStart),
+                        expanded = showExtraMenu,
+                        onDismissReq = {showExtraMenu = false},
+                        removeTitle = removeTitle,
+                        openDetails = openDetails
+                    )
+                }
             }
         }
-
-        TitleDropDownMenu(
-            showExtraMenu,
-            {showExtraMenu = false},
-            removeTitle,
-            openDetails
-        )
     }
 }
 
 @Composable
 private fun TitleDropDownMenu(
+    modifier: Modifier = Modifier,
     expanded: Boolean,
     onDismissReq: () -> Unit,
     removeTitle: () -> Unit,
     openDetails: () -> Unit
 ) {
     DropdownMenu(
+        modifier = modifier,
         expanded = expanded,
         onDismissRequest = onDismissReq
     ) {
         DropdownMenuItem(
-            text = { Text("Remove") },
+            modifier = Modifier.padding(4.dp),
+            text = {
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.Delete, null)
+                    Text("Remove")
+                }
+            },
             onClick = {
                 removeTitle()
                 onDismissReq()
@@ -101,7 +130,16 @@ private fun TitleDropDownMenu(
         )
 
         DropdownMenuItem(
-            text = { Text("Details") },
+            modifier = Modifier.padding(4.dp),
+            text = {
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.Info, null)
+                    Text("Details")
+                }
+            },
             onClick = {
                 openDetails()
                 onDismissReq()
