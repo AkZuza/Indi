6727dd44636a504ba168a2a4e46477b132275a6c
AkZuza <akhiljose@gmail.com>
2025-08-07 09:06:37 +0530

    change from custom composable to DockedSearchBar

--- composeApp/src/commonMain/kotlin/com/akzuza/indi/screens/home/IndiHome.kt
+++ composeApp/src/commonMain/kotlin/com/akzuza/indi/screens/home/IndiHome.kt
@@ -13,8 +13,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +35,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.akzuza.indi.common.FilePicker
@@ -37,9 +50,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.math.exp
 
-private val searchBarShape = RoundedCornerShape(50.dp)
+private val searchBarShape = RoundedCornerShape(15.dp)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IndiHome(
     state: IndiHomeState,
@@ -78,18 +93,84 @@ fun IndiHome(
     ) { padding ->
 
         var searchFilter by remember { mutableStateOf("") }
+        var expanded by remember { mutableStateOf(false) }
         val lazyListState = rememberLazyListState()
 
         Column (
             modifier = Modifier.fillMaxSize().padding(padding).padding(8.dp)
         ) {
 
-            IndiTextField(
-                modifier = Modifier.fillMaxWidth(),
-                text = searchFilter,
-                onUpdate = { searchFilter = it },
-                shape = searchBarShape
-            )
+            DockedSearchBar(
+                inputField = {
+                    TextField(
+                        value = searchFilter,
+                        onValueChange = {
+                            searchFilter = it
+                            if (searchFilter.isNotEmpty() && !expanded) {
+                                expanded = true
+                            }
+                        },
+                        placeholder = {
+                            Text("Search")
+                        },
+                        trailingIcon = {
+                            if (searchFilter.isNotEmpty()) {
+                                IconButton(
+                                    onClick = {
+                                        searchFilter = ""
+                                        expanded = false
+                                    }
+                                ) {
+                                    Icon(Icons.Default.Clear, contentDescription = null)
+                                }
+                            }
+                        },
+                        singleLine = true,
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                expanded = expanded,
+                onExpandedChange = { e: Boolean ->
+                    expanded = e
+                },
+                shape = searchBarShape,
+                tonalElevation = 4f.dp,
+                shadowElevation = 8f.dp,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                val filteredTitles = state.titles.filter { title -> title.title.contains(searchFilter) }
+                if(searchFilter.isNotEmpty() && filteredTitles.isNotEmpty()) {
+                    Box (
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth(),
+                            state = lazyListState,
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            contentPadding = PaddingValues(all = 8.dp)
+                        ) {
+
+                            items(filteredTitles.size) { i ->
+                                val title = filteredTitles[i]
+
+                            }
+                        }
+
+                        PlatformVerticalScrollBar(modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(), lazyListState)
+                    }
+                } else {
+                    ListItem(
+                        headlineContent = { Text("No title found for query $searchFilter")}
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.height(8.dp))
 
